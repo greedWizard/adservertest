30d1bf4c47d4836a797eac747be0b572b2d2b508
Emil <emilkhazioff@yandex.com>
2022-08-01 23:43:55 +0500

    little fix

--- adserver/board/views.py
+++ adserver/board/views.py
@@ -1,9 +1,9 @@
 from django.core.cache import cache
-from rest_framework import status
+
 from django.conf import settings
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import generics
+from rest_framework import generics, status
 from django_filters import rest_framework as filters
 from rest_framework.exceptions import PermissionDenied
 
